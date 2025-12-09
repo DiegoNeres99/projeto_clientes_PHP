d28ff70691a74c3d06598a6ced3f5e8de69f4fc8
DiegoNeres99 <miottadiego@gmail.com>
2025-12-08 22:16:44 -0300

    correção de rotas

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
 
+Route::get('/', function () {
+    return redirect()->route('clientes.index');
+});
+
 Route::resource('clientes', ClienteController::class);
